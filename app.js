208c229dabc2f41724439a8acaac33473f0ec62d
Navin004 <paudelpravin7@gmail.com>
2024-08-30 10:27:13 +0530

    app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+// Placeholder for JavaScript functionalities
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Online Banking System is ready!");
+    
+    // Add your JavaScript functionalities here
+});
